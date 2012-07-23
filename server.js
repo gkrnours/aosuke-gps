39f7fff9e27ffda7724e3fe487532ebf73095b0a
gkr@sylphe <gkr@jellycopter.net>
2012-07-23 11:17:15 +0200

    debugging

--- server.js
+++ server.js
@@ -2,10 +2,9 @@
  * Module dependencies.
  */
 
-console.log(typeof(fs))
 var express = require("express")
 var template= require("consolidate")
-var routes  = require("./routes.js")
+var routes  = require("\./routes.js")
 var http = require("http")
 
 var app = express()
