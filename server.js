7db2f683bb378abf234f42b365fcabdf3352dea2
gkr@sylphe <gkr@jellycopter.net>
2012-07-23 10:28:11 +0200

    Setting up Aosuke

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (aosuke) is running..');
-}).listen(9041);
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+var template= require('consolidate')
+var routes  = require('./routes.js')
+var http = require('http')
+
+var app = express()
+var views_dir = __dirname+"/data/views"
+
+app.configure(function(){
+	require("swig").init({cache: false, root: views_dir})
+	app.engine("html", template.swig)
+	app.set("view engine", "html")
+	app.set("views", views_dir)
+
+	app.use(express.static(__dirname + '/public'));
+	app.use("/css", express.static(__dirname+"/data/css"))
+	app.use("/img", express.static(__dirname+"/data/img"))
+	app.use("/js",  express.static(__dirname+"/data/js"))
+
+	app.use(express.favicon("data/img/favicon.ico"));
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(process.env["port"] || 3000, function() {
+	console.log("Express server listening on port " + app.get('port'));
+});
