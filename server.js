4676dd6be55bd7b340e2b56cfaaf7b8cbfefdf12
Coues Ludovic <gkr@jellycopter.net>
2013-07-31 23:15:49 +0200

    oops

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.configure(function(){
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.cookieParser("cherry pie"));
-	app.use(express.session({store: new rsStore({host:nv.RSPORT,pass:nv.RSPASS}),
+	app.use(express.session({store: new rsStore({host:nv.RSPORT,pass:nv.RSAUTH}),
 	          secret:"cherry pie", cookie: {maxAge: 180*1000}}));
 	app.use(app.router);
 	app.use(routes.err.gotcha) // check if the error is known
