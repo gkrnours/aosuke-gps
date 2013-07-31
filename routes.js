396b27fc82ac3076163b80d1d3f006db44c430cc
Coues Ludovic <gkr@jellycopter.net>
2013-07-31 23:08:04 +0200

    grbl

--- routes.js
+++ routes.js
@@ -12,9 +12,9 @@ this.setup = function setup(app){
 	app.post("/login/post", main.log.digest, main.log.xml, main.log.reg)
 
 	app.get("/", main.home)
-	app.get("/map", map.loadMe, map.render, map.generic)
-	app.get("/world/:id?", map.gather, map.load, map.render, map.generic)
-	app.post("/world", map.search)
+//	app.get("/map", map.loadMe, map.render, map.generic)
+//	app.get("/world/:id?", map.gather, map.load, map.render, map.generic)
+//	app.post("/world", map.search)
 	app.get("/people/:id?", main.home)
 }
 
