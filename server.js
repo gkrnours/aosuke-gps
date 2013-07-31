555c6c44d2cf70050400ddc7c7a435e6587ef2af
Coues Ludovic <gkr@jellycopter.net>
2013-07-31 23:21:04 +0200

    oops

--- server.js
+++ server.js
@@ -22,14 +22,13 @@ app.configure(function(){
 	app.use("/css", express.static(__dirname+"/data/css"))
 	app.use("/img", express.static(__dirname+"/data/img"))
 	app.use("/js",  express.static(__dirname+"/data/js"))
-
 	app.use(express.favicon("data/img/favicon.ico"));
+
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.cookieParser("cherry pie"));
-	console.log("rsStore: "+nv.RSPORT+":"+nv.RSAUTH)
-	app.use(express.session(secret:"cherry pie", cookie: {maxAge: 180*1000}}));
+	app.use(express.session({secret:"cherry pie", cookie: {maxAge: 180*1000}}));
 	app.use(app.router);
 	app.use(routes.err.gotcha) // check if the error is known
 	app.use(routes.err.generic)// throw pretty error at the user
