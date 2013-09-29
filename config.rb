be6faf36d19e69f8a4a5af584abb9a7fe9e0c034
Coues Ludovic <gkr@jellycopter.net>
2013-09-29 18:19:19 +0200

    touch screen friendly

---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path = "/"
+css_dir = "public/css"
+sass_dir = "public/css/sass"
+images_dir = "public/img"
+javascripts_dir = "public/js"
+output_style = :expanded
+line_comments = false
--- server.js
+++ server.js
@@ -2,44 +2,35 @@
  * Module dependencies.
  */
 
-var express = require("express")
-var template= require("consolidate")
-var rsStore = require("connect-redis")(express)
+var express = require('express')
+var swig    = require('swig')
+var http    = require('http')
+var path    = require('path')
+var rsStore = require('connect-redis')(express)
 var routes  = require("./routes.js")
-var http = require("http")
 
 var app = express()
-var views_dir = __dirname+"/data/views"
 var nv = process.env
 
-app.configure(function(){
-	require("swig").init({cache: false, root: views_dir})
-	app.engine("html", template.swig)
-	app.set("view engine", "html")
-	app.set("views", views_dir)
-
-	app.use(express.static(__dirname + '/public'));
-	app.use("/css", express.static(__dirname+"/data/css"))
-	app.use("/img", express.static(__dirname+"/data/img"))
-	app.use("/js",  express.static(__dirname+"/data/js"))
-	app.use(express.favicon("data/img/favicon.ico"));
-
-	app.use(express.logger('dev'));
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.cookieParser("cherry pie"));
-	app.use(express.session({secret:"cherry pie", cookie: {maxAge: 180*1000}}));
-	app.use(app.router);
-	app.use(routes.err.gotcha) // check if the error is known
-	app.use(routes.err.generic)// throw pretty error at the user
-});
-
-app.configure('development', function(){
-	app.use(express.errorHandler());
-});
+app.engine("html", swig.renderFile)
+app.set('views', __dirname + "/views")
+app.set('view engine', "html")
+app.set('view cache', false)
+swig.setDefaults({cache: false})
+app.set('port', nv.PORT || 8080)
+app.use(express.favicon("public/img/favicon.ico"));
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser("cherry pie"));
+app.use(express.session({secret:"cherry pie", cookie: {maxAge: 180*1000}}));
+app.use(app.router);
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(routes.err.gotcha) // check if the error is known
+app.use(routes.err.generic)// throw pretty error at the user
 
 routes.setup(app)
 
-http.createServer(app).listen(nv["port"] || 3000, function() {
+http.createServer(app).listen(app.get('port'), function() {
 	console.log("Express server listening on port " + app.get('port'));
 });
